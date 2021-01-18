21097e936bbd16a4a1a2912301deeeae7da22e42
Jae <dezcao@gmail.com>
2021-01-18 11:47:54 +0900

    empty folder delete for linux

--- middleware/index.js
+++ middleware/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 console.log(__dirname);
 const uses = fs.readdirSync(`${__dirname}/use`);
-const others = fs.readdirSync(`${__dirname}/others`);
+// const others = fs.readdirSync(`${__dirname}/others`);
 const customs = fs.readdirSync(`${__dirname}/customs`);
 
 module.exports = (app) => {
